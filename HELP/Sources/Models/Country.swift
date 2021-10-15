d7c0d7473b629aea3a02db6b6a5e8a37aab7d176
dhmgmhw <dhmgmhw@gmail.com>
2021-10-15 17:43:36 +0900

    chore: _ 자잘구리한 수정

--- HELP/Sources/Models/Country.swift
+++ HELP/Sources/Models/Country.swift
@@ -21,6 +21,21 @@ struct Country: Codable {
 //    let imgFlag: String?
 //}
 
-enum Countries {
-    case KR
-}
+var countries = [
+    ["nationalCode":"KR", "name":"대한민국"],
+    ["nationalCode":"AU", "name":"호주"],
+    ["nationalCode":"CA", "name":"캐나다"],
+    ["nationalCode":"CN", "name":"중국"],
+    ["nationalCode":"DE", "name":"독일"],
+    ["nationalCode":"GB", "name":"영국"],
+    ["nationalCode":"HK", "name":"홍콩"],
+    ["nationalCode":"ID", "name":"인도네시아"],
+    ["nationalCode":"JP", "name":"일본"],
+    ["nationalCode":"MY", "name":"말레이시아"],
+    ["nationalCode":"PH", "name":"필리핀"],
+    ["nationalCode":"SG", "name":"싱가포르"],
+    ["nationalCode":"TH", "name":"태국"],
+    ["nationalCode":"TW", "name":"타이완"],
+    ["nationalCode":"US", "name":"미국"],
+    ["nationalCode":"VN", "name":"베트남"],
+]
