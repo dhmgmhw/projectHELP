b9103443485069ef5d4d37c8cb9bfd0220485709
dhmgmhw <dhmgmhw@gmail.com>
2021-10-18 19:55:58 +0900

    Refactor : _ Api url singletonized

---
+++ HELP/Resources/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  HELP
+//
+//  Created by Moon on 2021/10/18.
+//
+
+import Foundation
+
+enum API {
+    static let MockURL: String = "https://6155639c93e3550017b08978.mockapi.io"
+}
