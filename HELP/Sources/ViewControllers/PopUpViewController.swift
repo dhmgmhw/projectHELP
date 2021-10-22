333a157addec027b6abfb4aabf5d1bbb5e12cad2
dhmgmhw <dhmgmhw@gmail.com>
2021-10-22 16:57:53 +0900

    Chore : _ outlet variables setting..

--- HELP/Sources/ViewControllers/PopUpViewController.swift
+++ HELP/Sources/ViewControllers/PopUpViewController.swift
@@ -8,14 +8,19 @@
 import UIKit
 
 class PopUpViewController: UIViewController {
-
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var imgFlag: UIImageView!
+    @IBOutlet weak var lblPoliceNumber: UILabel!
+    @IBOutlet weak var lblEmergencyNumber: UILabel!
+    @IBOutlet weak var lblAmbassyNumber: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
     @IBAction func btnSetTapped(_ sender: UIButton) {
-        self.view.window!.rootViewController?.dismiss(animated: true, completion: nil)
+        self.view.window?.rootViewController?.dismiss(animated: true, completion: nil)
     }
 
 }
