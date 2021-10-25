1f2f9a0917c6f91201c925a7c4320db36a586133
dhmgmhw <dhmgmhw@gmail.com>
2021-10-25 19:50:22 +0900

    Test : _ Alamofire test code

--- HELP/Sources/ViewControllers/HomeViewController.swift
+++ HELP/Sources/ViewControllers/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class HomeViewController: UIViewController {
     @IBOutlet weak var lblCurrentCountry: UILabel!
@@ -18,6 +19,24 @@ class HomeViewController: UIViewController {
         super.viewWillAppear(animated)
         guard let country = UserDefaults.standard.string(forKey: "nationName") else { return }
         lblCurrentCountry.text = country
+        
+
     }
     
+    func getTest() {
+            let url = "https://6155639c93e3550017b08978.mockapi.io/countries"
+            AF.request(url,
+                       method: .get,
+                       parameters: nil,
+                       encoding: URLEncoding.default,
+                       headers: ["Content-Type":"application/json", "Accept":"application/json"])
+                .validate(statusCode: 200..<300)
+                .responseJSON { (json) in
+                    print(json)
+            }
+        }
+    
+    @IBAction func btnConnectionTestTapped(_ sender: UIButton) {
+        getTest()
+    }
 }
