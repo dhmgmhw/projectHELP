375c5cc63e284c47e995198897bc5ab73f851d92
dhmgmhw <dhmgmhw@gmail.com>
2021-11-10 14:04:25 +0900

    Feat : 설정 초기데이터
    UserDefaults에 아무 데이터도 없는 경우(어플을 처음 설치했을때 등)에 초기 국가설정을 대한민국으로 설정

--- HELP/Sources/ViewControllers/BaseTabBarController.swift
+++ HELP/Sources/ViewControllers/BaseTabBarController.swift
@@ -15,7 +15,15 @@ class BaseTabBarController: UITabBarController {
         let nationData = UserDefaults.standard.string(forKey: "nationName")
         if nationData != nil {
             defaultIndex = 1
+            selectedIndex = defaultIndex
+            return
         }
+        
+        UserDefaults.standard.set("대한민국", forKey: "nationName")
+        UserDefaults.standard.set("112", forKey: "policeNumber")
+        UserDefaults.standard.set("119", forKey: "emergencyNumber")
+        UserDefaults.standard.set("0232100404", forKey: "embassyNumber")
+        UserDefaults.standard.set("서울특별시 서초구 남부순환로 2558 외교타운 6층 외교부 영사민원실", forKey: "embassyAddress")
         selectedIndex = defaultIndex
     }
 }
